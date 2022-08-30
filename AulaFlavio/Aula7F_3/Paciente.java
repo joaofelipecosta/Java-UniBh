62531d7a8291cc26693e2c5d6ff17d49b57664f1
João Felipe Costa <86801986+joaofelipecosta@users.noreply.github.com>
2022-08-30 17:00:32 -0300

    Create Paciente.java

---
+++ AulaFlavio/Aula7F_3/Paciente.java
@@ -0,0 +1,22 @@
+package aula7f_3;
+
+/**
+ *
+ * @author joaofelipe
+ */
+public class Paciente {
+    public String nome;
+    public String endereco;
+    public String cpf;
+    public Double peso;
+    public String identidade;
+    
+    public Paciente(String a, String b, String c, Double d, String e)
+    { nome = a;
+    endereco = b;
+    cpf = c;
+    peso = d;
+    identidade = e;
+    }
+    
+}
