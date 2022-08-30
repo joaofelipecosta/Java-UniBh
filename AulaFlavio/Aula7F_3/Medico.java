a3ff3aeafa07f531cd6a2340407d25e9e0bcc0c1
João Felipe Costa <86801986+joaofelipecosta@users.noreply.github.com>
2022-08-30 16:59:59 -0300

    Create Medico.java

---
+++ AulaFlavio/Aula7F_3/Medico.java
@@ -0,0 +1,17 @@
+package aula7f_3;
+
+/**
+ * @author joaofelipe
+ */
+public class Medico {
+    public String nome;
+    public String crm;
+    public String contato;
+    
+
+public Medico(String a, String b, String c)
+{ nome = a;
+crm = b;
+contato = c;
+}
+}
